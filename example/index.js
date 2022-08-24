1e6c5085adefc2f506908838e2ae0dd3246bb295
Lsong <song940@gmail.com>
2022-08-24 19:36:35 +0800

    update

--- example/index.js
+++ example/index.js
@@ -1,12 +1,14 @@
-const douyin = require('..');
+import * as dy from '../index.js'
 
 (async () => {
 
-    const dy = douyin();
-    const link = `https://v.douyin.com/VxENC1/`;
-    const full_link = await dy.shortcut(link);
-    const vid = full_link.split('/')[5];
-    const video = await dy.get_post(vid);
-    console.log(video);
+    // const link = `https://v.douyin.com/VxENC1/`;
+    // const full_link = await dy.shortcut(link);
+    // const vid = full_link.split('/')[5];
+    // const video = await dy.get_post(vid);
+    // console.log(video);
+
+    const user = await dy.get_video_list_by_user_id('MS4wLjABAAAAWiOs23d6NtmiUg98zONd6wQhmPsy1WLwZn0jEaCbDL8')
+    console.log(user);
 
 })();
--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const assert = require('assert');
-const https = require('https');
+import https from 'https'
+import assert from 'assert';
 
 const get = url => 
   new Promise(done => https.get(url, done));
@@ -14,28 +14,36 @@ const readStream = stream => {
   });
 };
 
-const douyin = () => {
-  return {
-    get_post(vid) {
-      const url = `https://www.iesdouyin.com/web/api/v2/aweme/iteminfo/?item_ids=${vid}`;
-      return Promise
-        .resolve()
-        .then(() => get(url))
-        .then(readStream)
-        .then(JSON.parse)
-        .then(res => {
-          assert.equal(res.status_code, 0);
-          return res.item_list;
-        });
-    },
-    shortcut(code) {
-      const url = `https://v.douyin.com/${code}/`;
-      return Promise
-      .resolve()
-      .then(() => get(url))
-      .then(res => res.headers['location'])
-    }
-  };
+const readJSONStream = stream => 
+  readStream(stream).then(JSON.parse);
+
+const getJSON = url => get(url).then(readJSONStream);
+
+export const get_user_by_id = async uid => {
+  const url = `https://www.iesdouyin.com/web/api/v2/user/info/?sec_uid=${uid}`;
+  const body = await getJSON(url);
+  assert.equal(body.status_code, 0);
+  return body.user_info;
 };
 
-module.exports = douyin;
+export const get_video_by_id = async id => {
+  const url = `https://www.iesdouyin.com/web/api/v2/aweme/iteminfo/?item_ids=${vid}`;
+  const res = await getJSON(url);
+  assert.equal(res.status_code, 0);
+  return res.item_list;
+};
+
+export const get_video_id_by_url = async url => {
+  const res = await get(url);
+  const full_link = res.headers['location'];
+  const vid = full_link.split('/')[5];
+  return vid;
+};
+
+export const get_video_list_by_user_id = async uid => {
+  const count = 10;
+  const max_cursor = 0;
+  const url = `https://www.iesdouyin.com/web/api/v2/aweme/post/?sec_uid=${uid}&count=${count}&max_cursor=${max_cursor}`;
+  const body = await getJSON(url);
+  return body;
+};
